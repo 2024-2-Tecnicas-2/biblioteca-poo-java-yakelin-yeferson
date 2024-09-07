e1e5eadf5841db819c7b203cb661b199d391bbf3
JackieArizaLaMejor <arizajackelin9@gmail.com>
2024-09-07 17:51:00 -0500

    Realizando clase Revista

--- src/main/java/com/mycompany/biblioteca/java/Revista.java
+++ src/main/java/com/mycompany/biblioteca/java/Revista.java
@@ -1,5 +1,23 @@
 package com.mycompany.biblioteca.java;
 
-public class Revista {
-    // TODO: Aquí va el código que inicializa tu aplicación.
+public class Revista extends Publicacion {
+    
+    int NumeroRevistas;
+    String NombreRevista;
+
+    public Revista(int NumeroRevistas, String NombreRevista, String Titulo, int anoPublicacion) {
+        super(Titulo, anoPublicacion);
+        this.NumeroRevistas = NumeroRevistas;
+        this.NombreRevista = NombreRevista;
+    }
+    
+    //Overrida
+
+    @Override
+    public String toString() {
+        return "Revista{"+ "Titulo=" + super.Titulo + ", anoPublicacion=" + super.anoPublicacion + "NumeroRevistas=" + NumeroRevistas + ", NombreRevista=" + NombreRevista + '}';
+    }
+    
+    
+   
 }
