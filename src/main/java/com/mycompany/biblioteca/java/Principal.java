e47376f4c7ca6d4315c427d4ba622222a4183c81
JackieArizaLaMejor <arizajackelin9@gmail.com>
2024-09-07 18:10:41 -0500

    Realizando metodo mostrar y editar

--- src/main/java/com/mycompany/biblioteca/java/Principal.java
+++ src/main/java/com/mycompany/biblioteca/java/Principal.java
@@ -9,26 +9,38 @@ public class Principal {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        
+        MostrarPublicaciones();
 
     }
-    
-    public static void agregarPublicacion(){
-        
+
+    public static void agregarPublicacion() {
+
         int opc = Integer.parseInt(sc.nextLine());
-        
+
         switch (opc) {
             case 1:
                 System.out.println("Ingrese el nombre de la revista: ");
-                
-                
+
                 break;
             default:
                 throw new AssertionError();
         }
     }
+
+    public static void MostrarPublicaciones() {
+        for (int i = 0; i < publicaciones.size(); i++) {
+            System.out.println(i + " "+ publicaciones.get(i).toString());
+        }
+
+    }
     
+    public static void EditarPublicaciones(){
+        MostrarPublicaciones();
+        
+        System.out.println("Ingresa el que quieras editar");
+        
+  
     
-    
-    
+    }
+
 }
