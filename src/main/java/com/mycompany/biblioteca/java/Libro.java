6cc7c5abd1300a7fdaeb8acfe4f5b2e071736aa9
JackieArizaLaMejor <arizajackelin9@gmail.com>
2024-09-07 17:47:37 -0500

    Realizando clase libro

--- src/main/java/com/mycompany/biblioteca/java/Libro.java
+++ src/main/java/com/mycompany/biblioteca/java/Libro.java
@@ -1,5 +1,28 @@
 package com.mycompany.biblioteca.java;
 
-public class Libro {
-    // TODO: Aquí va el código que inicializa tu aplicación.
+public class Libro extends Publicacion {
+    
+    String Autor;
+    int Numero_de_paginas;
+    
+
+    //Contructor
+
+    public Libro(String Autor, int Numero_de_paginas, String Titulo, int anoPublicacion) {
+        super(Titulo, anoPublicacion);
+        this.Autor = Autor;
+        this.Numero_de_paginas = Numero_de_paginas;
+    }
+    
+    //Override
+
+    @Override
+    public String toString() {
+        return "Libro{"+ "Titulo=" + super.Titulo + ", anoPublicacion=" + super.anoPublicacion + "Autor=" + Autor + ", Numero_de_paginas=" + Numero_de_paginas + '}';
+    }
+    
+    
+    
+    
+    
 }
