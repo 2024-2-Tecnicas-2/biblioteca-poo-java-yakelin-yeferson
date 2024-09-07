05b526069f03e38195de731140e17a41612036a5
YefersonL <linaresjefferson1234@gmail.com>
2024-09-07 18:09:46 -0500

    Metodo de agregar publicacion

--- src/main/java/com/mycompany/biblioteca/java/Principal.java
+++ src/main/java/com/mycompany/biblioteca/java/Principal.java
@@ -9,17 +9,30 @@ public class Principal {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
+        System.out.println("");
         
 
     }
     
-    public static void agregarPublicacion(){
+    public static void agregarPublicacion(int opc){
         Revista rv = new Revista();
-        int opc = Integer.parseInt(sc.nextLine());
+        
         
         switch (opc) {
             case 1:
+                System.out.println("Ingrese el titulo de la revista: ");
+                String Nomb = sc.nextLine();
+                rv.setTitulo(Nomb);
+                System.out.println("Ingrese el año de publicacion: ");
+                int ano = Integer.getInteger(sc.nextLine());
+                rv.setAnoPublicacion(ano);
+                System.out.println("Ingrese el numero de revista: ");
+                int numbrevista = Integer.getInteger(sc.nextLine());
+                rv.setNumeroRevistas(numbrevista);
                 System.out.println("Ingrese el nombre de la revista: ");
+                String nombRevista = sc.nextLine();
+                rv.setNombreRevista(nombRevista);
+                publicaciones.add(rv);
                 
                 
                 
