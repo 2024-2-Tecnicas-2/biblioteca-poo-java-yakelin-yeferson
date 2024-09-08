885042e7f3bc12312e86ab189f092d143f32e529
YefersonL <linaresjefferson1234@gmail.com>
2024-09-07 19:30:47 -0500

    Finalizado MAIN

--- src/main/java/com/mycompany/biblioteca/java/Biblioteca.java
+++ src/main/java/com/mycompany/biblioteca/java/Biblioteca.java
@@ -5,23 +5,32 @@ import java.util.Scanner;
 
 public class Biblioteca {
 
-    static ArrayList<Publicacion> publicaciones = new ArrayList<>();
+    static ArrayList publicaciones = new ArrayList<>();
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         int opc;
         while (true) {
-            System.out.println("1. Agregar publicacion: ");
-            System.out.println("Mostrar Publicaciones");
+            System.out.println("1.Agregar publicacion: ");
+            System.out.println("2.Mostrar Publicaciones: ");
+            System.out.println("3.Editar publicacion: ");
             opc = Integer.parseInt(sc.nextLine());
-
+            if(opc ==1){
+                agregarPublicacion(opc);
+            }else if(opc ==2){
+                MostrarPublicaciones(opc);
+                
+            }else if(opc ==3){
+                EditarPublicaciones(opc);
+            
+            }else System.out.println("Ingrese una opcion correcta");
         }
 
     }
 
-    public static void agregarPublicacion() {
+    public static void agregarPublicacion(int opc) {
         Revista rv = new Revista();
-        int opc = Integer.parseInt(sc.nextLine());
+        
 
         switch (opc) {
             case 1:
@@ -29,10 +38,10 @@ public class Biblioteca {
                 String Nomb = sc.nextLine();
                 rv.setTitulo(Nomb);
                 System.out.println("Ingrese el año de publicacion: ");
-                int ano = Integer.getInteger(sc.nextLine());
+                int ano = Integer.parseInt(sc.nextLine());
                 rv.setAnoPublicacion(ano);
                 System.out.println("Ingrese el numero de revista: ");
-                int numbrevista = Integer.getInteger(sc.nextLine());
+                int numbrevista = Integer.parseInt(sc.nextLine());
                 rv.setNumeroRevistas(numbrevista);
                 System.out.println("Ingrese el nombre de la revista: ");
                 String nombRevista = sc.nextLine();
@@ -41,22 +50,24 @@ public class Biblioteca {
 
                 break;
             case 2:
-                MostrarPublicaciones();
+                MostrarPublicaciones(opc);
                 break;
+            case 3:
+                EditarPublicaciones(opc);
             default:
                 throw new AssertionError();
         }
     }
 
-    public static void MostrarPublicaciones() {
+    public static void MostrarPublicaciones(int opc) {
         for (int i = 0; i < publicaciones.size(); i++) {
             System.out.println(i + " : " + publicaciones.get(i).toString());
         }
 
     }
 
-    public static void EditarPublicaciones() {
-        MostrarPublicaciones();
+    public static void EditarPublicaciones(int opc) {
+        MostrarPublicaciones(opc);
 
         System.out.println("Ingresa el que quieras editar");
 
@@ -71,7 +82,7 @@ public class Biblioteca {
 
         }
 
-        Publicacion publicacion = publicaciones.get(index);
+        Publicacion publicacion = (Publicacion) publicaciones.get(index);
 
         System.out.println("Introduce el nuevo titulo");
         String nuevoTitulo = sc.nextLine();
